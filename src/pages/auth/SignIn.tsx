4a5b15fb2f26ff39c1161a37bf1ad524d28328e5
chetramaaseri <developer.chetramaaseri@gmail.com>
2025-03-11 11:41:36 +0530

    dark mode fix

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -4,7 +4,7 @@ const SignIn = () => {
 
     return (
         <>
-            <section className="bg-gray-50 dark:bg-gray h-[calc(100vh-70px)]">
+            <section className="bg-gray-50 dark:bg-gray-900 h-[calc(100vh-70px)]">
                 <div className="flex flex-col items-center justify-center px-6 py-10 mx-auto md:h-full lg:py-0">
                     <a href="#" className="hidden md:flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
